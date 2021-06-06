679d1f6847607bda9a7c91adc502409ab0a66960
Ray Jeong <rayrayj92@gmail.com>
2021-06-06 15:23:41 +0900

    시간 별 기능

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,12 +3,14 @@ import styled from 'styled-components';
 import { wowDB } from 'shared/firebase';
 import { BarLineChart } from 'components/components';
 import { useAuth } from 'contexts/AuthProvider';
+import moment from 'moment';
 
 export default function Main() {
     const options = ['플레이 시간', '던전 횟수', '레이드 횟수'];
     const [record, setRecord] = useState<any>([]);
     const [filter, setFilter] = useState<string>('플레이 시간');
     const { currentUser } = useAuth();
+    const today = moment().format("MMM Do YY"); 
 
     function handleChange(e : React.ChangeEvent<HTMLSelectElement>) {
       setFilter(e.target.value);
@@ -26,6 +28,21 @@ export default function Main() {
 
     return (
       <MainContainer>
+        <DateContainer>
+          <Prev>
+            Prev
+          </Prev>
+          <DateList>
+            <li>1</li>
+            <li>2</li>
+            <li>{today}</li>
+            <li>4</li>
+            <li>5</li>
+          </DateList>
+          <Next>
+            Next
+          </Next>
+        </DateContainer>
         <FilterContainer>
           <select onChange={handleChange}>
             {options.map((option, index) => {
@@ -47,4 +64,28 @@ const MainContainer = styled.div`
 const FilterContainer = styled.div`
   display: flex;
   flex-direction: row-reverse;
+`;
+
+const DateContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const DateList = styled.ul`
+  display: contents;
+
+  & li {
+    display: inline;
+    margin: 0 1.25rem;
+  }
+`;
+
+const Prev = styled.div`
+  cursor: pointer;
+  margin: 0 1.25rem;
+`;
+
+const Next = styled.div`
+  cursor: pointer;
+  margin: 0 1.25rem;
 `;
