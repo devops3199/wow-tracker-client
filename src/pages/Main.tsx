b98d1cc4706882499bbbc330ec55e8afd1c2aa3a
Ray Jeong <rayrayj92@gmail.com>
2021-06-10 19:27:09 +0900

    날짜 필터 적용

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import { wowDB } from 'shared/firebase';
 import { BarLineChart } from 'components/components';
@@ -10,15 +10,25 @@ export default function Main() {
     const [record, setRecord] = useState<any>([]);
     const [filter, setFilter] = useState<string>('플레이 시간');
     const { currentUser } = useAuth();
-    const date_from = moment().startOf("week").format("YYYY-MM-DD");
-    const date_to = moment().endOf("week").format("YYYY-MM-DD");
+    const [dateFrom, setDateFrom] = useState<string>(moment().startOf("week").format("YYYY-MM-DD"));
+    const [dateTo, setDateTo] = useState<string>(moment().endOf("week").format("YYYY-MM-DD"));
 
     function handleChange(e : React.ChangeEvent<HTMLSelectElement>) {
       setFilter(e.target.value);
     }
 
+    function handleFilter() {
+      wowDB.where('uid', '==', currentUser.uid).orderBy('date', 'asc').startAt(dateFrom).endAt(dateTo).get().then((docs) => {
+        const list:any = [];
+        docs.forEach((doc) => {
+          list.push({...doc.data(), id : doc.id});
+        })
+        setRecord(list);
+      }).catch((err) => console.log(err, "Error"));
+    }
+
     useEffect(() => {
-      wowDB.where('uid', '==', currentUser.uid).orderBy('date','asc').get().then((docs) => {
+      wowDB.where('uid', '==', currentUser.uid).orderBy('date','asc').startAt(dateFrom).endAt(dateTo).get().then((docs) => {
         const list:any = [];
         docs.forEach((doc) => {
           list.push({...doc.data(), id : doc.id});
@@ -32,11 +42,14 @@ export default function Main() {
         <DateContainer>
           <DateSelector>
             <label htmlFor="from">From</label>
-            <input type="date" id="from" value={date_from} />
+            <input type="date" id="from" value={dateFrom} onChange={(e : React.ChangeEvent<HTMLInputElement>) => setDateFrom(e.target.value)} />
           </DateSelector>
           <DateSelector>
             <label htmlFor="to">To</label>
-            <input type="date" id="to" value={date_to} />
+            <input type="date" id="to" value={dateTo} onChange={(e : React.ChangeEvent<HTMLInputElement>) => setDateTo(e.target.value)} />
+          </DateSelector>
+          <DateSelector>
+            <FilterButton onClick={handleFilter}>필터 적용</FilterButton>
           </DateSelector>
         </DateContainer>
         <FilterContainer>
@@ -73,4 +86,8 @@ const DateSelector = styled.div`
   & label {
     margin: 0 1rem;
   }
+`;
+
+const FilterButton = styled.button`
+
 `;
